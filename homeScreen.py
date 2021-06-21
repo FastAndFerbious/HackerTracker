4ebdffc4c30c4c25accbb7554366e896b1e61ca8
chirag0116 <chiragnarang16@gmail.com>
2021-06-21 11:41:18 -0400

    updated calendar

--- homeScreen.py
+++ homeScreen.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+from tkcalendar import Calendar
 
 '''def homeScreen():
     window = Tk()
@@ -34,24 +35,27 @@ class HomePage(Page):
 class Page2(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
-        lbl = Label(self, text="Please enter today's date:  ", font=("Arial Bold", 20))
+        lbl = Label(self, text="Please select today's date:  ", font=("Arial Bold", 20))
         lbl.grid(column=0, row=0, sticky="", columnspan=3)
 
+        cal = Calendar(self, selectmode="day", year=2021, month=6, day=21)
+        cal.place(relx=.45, rely=.5, anchor="c")
+
         #create spins to add date
-        month = Label(self, text="Month")
-        month.grid(column=0, row=1, sticky="")
-        spin = Spinbox(self, from_=1, to=12, width=5, format="%02.0f")
-        spin.grid(column=0, row=2, sticky="")
+        #month = Label(self, text="Month")
+        #month.grid(column=0, row=1, sticky="")
+        #spin = Spinbox(self, from_=1, to=12, width=5, format="%02.0f")
+        #spin.grid(column=0, row=2, sticky="")
 
-        day = Label(self, text="Day")
-        day.grid(column=1, row=1, sticky="")
-        spin2 = Spinbox(self, from_=1, to=30, width=5, format="%02.0f")
-        spin2.grid(column=1, row=2, sticky="")
+        #day = Label(self, text="Day")
+        #day.grid(column=1, row=1, sticky="")
+        #spin2 = Spinbox(self, from_=1, to=30, width=5, format="%02.0f")
+        #spin2.grid(column=1, row=2, sticky="")
 
-        year = Label(self, text="Year")
-        year.grid(column=2, row=1, sticky="")
-        spin3 = Spinbox(self, from_=0000, to=9999, width=5, format="%04.0f")
-        spin3.grid(column=2, row=2, sticky="")
+        #year = Label(self, text="Year")
+        #year.grid(column=2, row=1, sticky="")
+        #spin3 = Spinbox(self, from_=0000, to=9999, width=5, format="%04.0f")
+        #spin3.grid(column=2, row=2, sticky="")
 
 #Third page asking to select options
 class Page3(Page):
