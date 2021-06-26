f93089310e0d66600373a8efe93aad8008443a93
dhrubo28 <58522980+dhrubo28@users.noreply.github.com>
2021-06-26 12:47:44 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+# https://towardsdatascience.com/synonyms-and-antonyms-in-python-a865a5e14ce8
+# https://spacytextblob.netlify.app/docs/example
+import nltk
+import spacy
+from nltk.corpus import wordnet
+from spacytextblob.spacytextblob import SpacyTextBlob
+nltk.download('wordnet')
+
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe("spacytextblob")
+
+
+print("Input one or two sentences")
+text = input()
+
+# Input single text?
+doc = nlp(text)
+
+# print(doc._.assessments)
+
+words = list(zip(*doc._.assessments))
+
+#for index in range(0, len(words)):
+word = list(zip(*words[0]))
+
+word3 = list(zip(*word))
+
+for index in range(0, len(word3)):
+    print(index, "-", *word3[index])
+    # Word Cloud
+    # Creating a list
+    synonyms = []
+    for syn in wordnet.synsets(*word3[index]):
+        for lm in syn.lemmas():
+            synonyms.append(lm.name())  # adding into synonyms
+    print(set(synonyms))
+
+
+# Input multiple lines of text?
+docs = list(nlp.pipe([text]))
+
+# for doc in docs:
+    # print('Assessments:', doc._.assessments)
+
+
