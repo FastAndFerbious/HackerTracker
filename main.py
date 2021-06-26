7497802486a607f9a166c135841892b93faabffd
dhrubo28 <58522980+dhrubo28@users.noreply.github.com>
2021-06-26 16:21:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,45 @@ import nltk
 import spacy
 from nltk.corpus import wordnet
 from spacytextblob.spacytextblob import SpacyTextBlob
+
 nltk.download('wordnet')
 
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe("spacytextblob")
 
 
-print("Input one or two sentences")
-text = input()
+def main():
+    print("Input one or two sentences")
+    text = input()
 
-# Input single text?
-doc = nlp(text)
+    # Input single text?
+    doc = nlp(text)
 
-# print(doc._.assessments)
+    # print(doc._.assessments)
 
-words = list(zip(*doc._.assessments))
+    # list comphresion to get the emotional words
+    words = list(zip(*doc._.assessments))
+    # for index in range(0, len(words)):
+    word = list(zip(*words[0]))
+    word3 = list(zip(*word))
 
-#for index in range(0, len(words)):
-word = list(zip(*words[0]))
+    for index in range(0, len(word3)):
+        print(index, "-", *word3[index])
 
-word3 = list(zip(*word))
+        # Word Cloud
+        synonyms = []
+        for syn in wordnet.synsets(*word3[index]):
+            for lm in syn.lemmas():
+                synonyms.append(lm.name())  # adding into synonyms
 
-for index in range(0, len(word3)):
-    print(index, "-", *word3[index])
-    # Word Cloud
-    # Creating a list
-    synonyms = []
-    for syn in wordnet.synsets(*word3[index]):
-        for lm in syn.lemmas():
-            synonyms.append(lm.name())  # adding into synonyms
-    print(set(synonyms))
+        print(set(synonyms))
 
-
-# Input multiple lines of text?
-docs = list(nlp.pipe([text]))
-
-# for doc in docs:
-    # print('Assessments:', doc._.assessments)
+    # Input multiple lines of text?
+    docs = list(nlp.pipe([text]))
 
 
+    # for doc in docs:
+        # print('Assessments:', doc._.assessments)
+
+if __name__ == "__main__":
+    main()
