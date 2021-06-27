9dae6e76f1ac7abedb0d59957f4dfa4d3abef563
chirag0116 <chiragnarang16@gmail.com>
2021-06-27 00:04:03 -0400

    modified formatting and color scheme

--- homeScreen.py
+++ homeScreen.py
@@ -23,19 +23,22 @@ class Page(tk.Frame):
 #home page
 class HomePage(Page):
     def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        lbl = Label(self, text="Welcome to HackerTracker!", font=("Comic Sans MS", 50, 'bold'), fg='pink')
-        lbl.place(x=110, y=100)
+        Page.__init__(self, *args, **kwargs, bg="black")
+        lbl = Label(self, text="Welcome to HackerTracker!", font=("Comic Sans MS", 50, 'bold'), bg="black", fg="SpringGreen2")
+        lbl.place(relx=0.5, rely=0.5, anchor ="c")
 
 #Second page asking for date
 class Page2(Page):
     def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        lbl = Label(self, text="Please select today's date:  ",font=("Comic Sans MS", 40, 'bold'), fg='pink')
-        lbl.place(x=120, y=0)
+        Page.__init__(self, *args, **kwargs, bg="black")
+        lbl = Label(self, text="Please select today's date:  ",font=("Comic Sans MS", 40, 'bold'), bg="black", fg='SpringGreen2')
+        lbl.place(relx=.5, rely=.05, anchor="c")
 
-        cal = Calendar(self, selectmode="day", year=2021, month=6, day=21, selectforeground='pink', foreground='yellow', highlightcolor='pink', normalforeground='orange', font=("Comic Sans MS", 20))
-        cal.place(relx=.45, rely=.5, anchor="c", width=500, height=230)
+        #cal = Calendar(self, selectmode="day", year=2021, month=6, day=21, selectforeground='pink', foreground='yellow', highlightcolor='pink', normalforeground='orange', font=("Comic Sans MS", 20))
+        cal = Calendar(self, background="black", disabledbackground="black", bordercolor="black",
+                 headersbackground="black", normalbackground="black", foreground='white',
+                 normalforeground='white', headersforeground='white', font=("Comic Sans MS", 20))
+        cal.place(relx=.5, rely=.5, anchor="c")
 
         #create spins to add date
         #month = Label(self, text="Month")
@@ -56,73 +59,78 @@ class Page2(Page):
 #Third page asking to select options
 class Page3(Page):
     def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)                            #copy these 3 lines to make a new class
+        Page.__init__(self, *args, **kwargs, bg="black")                            #copy these 3 lines to make a new class
         #make checkbutton for multiselect
-        lbl = Label(self, text="Select desired categories", font=("Comic Sans MS", 40, 'bold'), fg='pink')
-        lbl.place(x=120, y=0)
+        lbl = Label(self, text="Select desired categories", font=("Comic Sans MS", 40, 'bold'), bg="black", fg='SpringGreen2')
+        lbl.place(relx=.5, rely=.05, anchor="c")
         sleep_state = BooleanVar()
-        sleep = Checkbutton(self, text="Sleep", var=sleep_state, font=("Comic Sans MS", 20), fg='orange')
-        sleep.place(x=20, y=100)
-        exercise = Checkbutton(self, text="Exercise", font=("Comic Sans MS", 20), fg='orange')
-        exercise.place(x=20, y=140)
-        caffeine = Checkbutton(self, text="Caffeine", font=("Comic Sans MS", 20), fg='orange')
-        caffeine.place(x=20, y=180)
-        mood = Checkbutton(self, text="Mood", font=("Comic Sans MS", 20), fg='orange')
-        mood.place(x=20, y=220)
-        confidence = Checkbutton(self, text="Confidence", font=("Comic Sans MS", 20), fg='orange')
-        confidence.place(x=160, y=100)
-        screenTime = Checkbutton(self, text="Screen Time", font=("Comic Sans MS", 20), fg='orange')
-        screenTime.place(x=160, y=140)
-        socializing = Checkbutton(self, text="Socializing", font=("Comic Sans MS", 20), fg='orange')
-        socializing.place(x=160, y=180)
-        productivity = Checkbutton(self, text="Productivity", font=("Comic Sans MS", 20), fg='orange')
-        productivity.place(x=160, y=220)
-        hygiene = Checkbutton(self, text="Hygiene", font=("Comic Sans MS", 20), fg='orange')
-        hygiene.place(x=320, y=100)
+        sleep = Checkbutton(self, text="Sleep", var=sleep_state, font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        sleep.place(relx=.28, rely=.2, anchor="c")
+        exercise = Checkbutton(self, text="Exercise", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        exercise.place(relx=.28, rely=.3, anchor="c")
+        caffeine = Checkbutton(self, text="Caffeine", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        caffeine.place(relx=.28, rely=.4, anchor="c")
+        mood = Checkbutton(self, text="Mood", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        mood.place(relx=.28, rely=.5, anchor="c")
+        confidence = Checkbutton(self, text="Confidence", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        confidence.place(relx=.48, rely=.2, anchor="c")
+        screenTime = Checkbutton(self, text="Screen Time", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        screenTime.place(relx=.48, rely=.3, anchor="c")
+        socializing = Checkbutton(self, text="Socializing", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        socializing.place(relx=.48, rely=.4, anchor="c")
+        productivity = Checkbutton(self, text="Productivity", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        productivity.place(relx=.48, rely=.5, anchor="c")
+        hygiene = Checkbutton(self, text="Hygiene", font=("Comic Sans MS", 20), bg="black", fg='white', highlightbackground ="SpringGreen2")
+        hygiene.place(relx=.68, rely=.2, anchor="c")
         #https://likegeeks.com/python-gui-examples-tkinter-tutorial/
 
 #Fourth Page prompting journaling input
 class Page4(Page):
      def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        choice_lbl = Label(self, text="Select Best Option", font=("Comic Sans MS", 50, 'bold'), fg='pink')
-        choice_lbl.place(x=140, y=0)
+        Page.__init__(self, *args, **kwargs, bg="black")
+        choice_lbl = Label(self, text="Select Best Option", font=("Comic Sans MS", 40, 'bold'), bg="black", fg='SpringGreen2')
+        choice_lbl.place(relx=.5, rely=.05, anchor="c")
 
-        sleep_label = Label(self, text="How many hours did you sleep last night?", font=("Comic Sans MS", 30, 'bold'), fg='yellow')
-        sleep_label.place(x=0, y=100)
+        sleep_label = Label(self, text="How many hours did you sleep last night?", font=("Comic Sans MS", 30, 'bold'), bg="black", fg='white')
+        sleep_label.place(relx=.5, rely=.25, anchor="c")
 
-        redbutton = Button(self, text="0-3 hours", fg="orange", padx=10, pady=20, font=("Comic Sans MS", 20), bg='yellow')
-        redbutton.pack(side=LEFT)
+        redbutton = Button(self, text="0-3 hours", fg="white", padx=10, pady=20, font=("Comic Sans MS", 20), bg='red')
+        #redbutton.pack(side=LEFT)
+        redbutton.place(relx=.25, rely=.5, anchor="c")
 
-        greenbutton = Button(self, text="3-5 hours", fg="orange", padx=10, pady=20, font=("Comic Sans MS", 20), bg='yellow')
-        greenbutton.pack(side=LEFT)
+        orangeredbutton = Button(self, text="3-5 hours", fg="white", padx=10, pady=20, font=("Comic Sans MS", 20), bg='orange red')
+        #greenbutton.pack(side=LEFT)
+        orangeredbutton.place(relx=.37, rely=.5, anchor="c")
 
-        bluebutton = Button(self, text="6-8 hours", fg="orange", padx=10, pady=20, font=("Comic Sans MS", 20), bg='yellow')
-        bluebutton.pack(side=LEFT)
+        orangebutton = Button(self, text="6-8 hours", fg="white", padx=10, pady=20, font=("Comic Sans MS", 20), bg='orange')
+        #bluebutton.pack(side=LEFT)
+        orangebutton.place(relx=.49, rely=.5, anchor="c")
 
-        purpbutton = Button(self, text="9-11 hours", fg="orange", padx=10, pady=20, font=("Comic Sans MS", 20), bg='yellow')
-        purpbutton.pack(side=LEFT)
+        yellowbutton = Button(self, text="9-11 hours", fg="white", padx=10, pady=20, font=("Comic Sans MS", 20), bg='yellow')
+        #purpbutton.pack(side=LEFT)
+        yellowbutton.place(relx=.618, rely=.5, anchor="c")
 
-        blackbutton = Button(self, text="11+ hours", fg="orange", padx=10, pady=20, font=("Comic Sans MS", 20), bg='yellow')
-        blackbutton.pack(side=LEFT)
+        greenbutton = Button(self, text="11+ hours", fg="white", padx=10, pady=20, font=("Comic Sans MS", 20), bg='green')
+        #blackbutton.pack(side=LEFT)
+        greenbutton.place(relx=.749, rely=.5, anchor="c")
 
 #Page 5 with plots
 class Page5(Page):
      def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        graph_lab = Label(self, text="Plots",  font=("Comic Sans MS", 50, 'bold'), fg='pink')
-        graph_lab.pack(pady=10, padx=10)
+        Page.__init__(self, *args, **kwargs, bg="black")
+        graph_lab = Label(self, text="Plots",  font=("Comic Sans MS", 40, 'bold'), bg="black", fg='SpringGreen2')
+        graph_lab.place(relx=.5, rely=.05, anchor="c")
 
 #NLP prompting user for input
 class Page6(Page):
     def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        graph_lab = Label(self, text="How are you feeling today:", font=("Arial Bold", 20))
-        graph_lab.pack(pady=10, padx=10)
+        Page.__init__(self, *args, **kwargs, bg="black")
+        graph_lab = Label(self, text="How are you feeling today:", font=("Comic Sans MS", 40, 'bold'), bg="black", fg='SpringGreen2')
+        graph_lab.place(relx=.5, rely=.05, anchor="c")
         E1 = Entry(self)
-        E1.pack(side=TOP)
-        blueButton = Button(self, text="Submit", fg="blue")
-        blueButton.pack(side=TOP)
+        E1.place(relx=.5, rely=.25, anchor="c")
+        blueButton = Button(self, text="Submit", bg="black", fg="white")
+        blueButton.place(relx=.5, rely=.35, anchor="c")
 
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
@@ -151,8 +159,8 @@ class MainView(tk.Frame):
         window.config(menu=menu)
 
         #make frames
-        button_frame = tk.Frame(self, bg="white")
-        container = tk.Frame(self)
+        button_frame = tk.Frame(self, bg="gray")
+        container = tk.Frame(self, bg="black")
         button_frame.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
         #create next button
@@ -162,12 +170,12 @@ class MainView(tk.Frame):
         back_btn = Button(button_frame, text="Back", bg="blue", command=lambda: self.goBack(num))
         back_btn.pack(side="left")
         #place screens into a container
-        home.place(in_=container, x=50, y=50, relwidth=1, relheight=1)
-        date.place(in_=container, x=50, y=50, relwidth=1, relheight=1)
-        options.place(in_=container, x=50, y=50, relwidth=1, relheight=1)
-        choices.place(in_=container, x=50, y=50, relwidth=1, relheight=1)
-        plots.place(in_=container, x=50, y=50, relwidth=1, relheight=1)
-        nlp.place(in_=container, x=50, y=50, relwidth=1, relheight=1)
+        home.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        date.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        options.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        choices.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        plots.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        nlp.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
         screens[0].show()
 
@@ -196,5 +204,5 @@ if __name__ == "__main__":
     main = MainView(window)
     main.pack(side="top", fill="both", expand=True)
     window.title("HackerTracker")
-    window.geometry('1000x400')
+    window.geometry('1200x600')
     window.mainloop()
