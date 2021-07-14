2eb746b194d04c500e88922c729c0fd66de7acf8
dhrubo28 <58522980+dhrubo28@users.noreply.github.com>
2021-07-14 15:09:17 -0400

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -13,16 +13,29 @@ nlp.add_pipe("spacytextblob")
 
 
 def nlpFunc(text):
-    synonyms = []
+    pos_synonyms = []
+    neu_synonyms = []
+    neg_synonyms = []
+
     words = []
-    neg_words = []
+    # neg_words = []
     neu_words = []
     neg_words = []
 
+    word = ""
+    polarity = 0.0
+
+    pos_polarity = dict()
+    neu_polarity = dict()
+    neg_polarity = dict()
+
     # NLP analysis of single-line text
     doc = nlp(text)
+    # doc = list(nlp.pipe([text]))
 
     # print(doc._.assessments)
+
+    # seperate out words
 
     if len(text) != 0:
 
@@ -30,12 +43,17 @@ def nlpFunc(text):
 
         if doc._.polarity > 0:
             words = list(zip(*doc._.assessments))
+            print(words)
+            # pos_polarity[doc._.assessments] = doc._.polarity
 
         elif doc._.polarity == 0:
             neu_words = list(zip(*doc._.assessments))
+            # neu_polarity[doc._.assessments] = doc._.polarity
 
         else:
             neg_words = list(zip(*doc._.assessments))
+            print(neg_words)
+            # neg_polarity[doc._.assessments] = doc._.polarity
 
         # for index in range(0, len(words)):
         if (len(words) != 0):
@@ -49,13 +67,14 @@ def nlpFunc(text):
 
                 # Word Cloud
                 # looks for synonym(s) of the emotional word(s)
-                for sy in wordnet.synsets(*word3[index]):
+                for syn in wordnet.synsets(*word3[index]):
                     # returns the synonyms of the emotional word(s)
-                    for le in sy.lemmas():
-                        # adds the snonym(s) to the synonyms list
-                        synonyms.append(le.name())
+                    for lm in syn.lemmas():
+                        if lm.name() not in pos_synonyms :
+                            # adds the snonym(s) to the synonyms list
+                            pos_synonyms.append(lm.name())
                 # prints the synonym(s) of the emotional word(s)
-                print("pos syn", set(synonyms))
+                print("pos syn", set(pos_synonyms))
 
         elif(len(neu_words) != 0):
 
@@ -68,13 +87,14 @@ def nlpFunc(text):
 
                 # Word Cloud
                 # looks for synonym(s) of the emotional word(s)
-                for sy in wordnet.synsets(*neu_words3[index]):
+                for syn in wordnet.synsets(*neu_words3[index]):
                     # returns the synonyms of the emotional word(s)
-                    for le in sy.lemmas():
-                        # adds the snonym(s) to the synonyms list
-                        synonyms.append(le.name())
+                    for lm in syn.lemmas():
+                        if lm.name() not in neu_synonyms:
+                            # adds the snonym(s) to the synonyms list
+                            neu_synonyms.append(lm.name())
                 # prints the synonym(s) of the emotional word(s)
-                print("neu syn", set(synonyms))
+                print("neu syn", set(neu_synonyms))
 
         elif len(neg_words) != 0:
 
@@ -87,16 +107,17 @@ def nlpFunc(text):
 
                 # Word Cloud
                 # looks for synonym(s) of the emotional word(s)
-                for sy in wordnet.synsets(*neg_words3[index]):
+                for syn in wordnet.synsets(*neg_words3[index]):
                     # returns the synonyms of the emotional word(s)
-                    for le in sy.lemmas():
-                        # adds the snonym(s) to the synonyms list
-                        synonyms.append(le.name())
+                    for lm in syn.lemmas():
+                        if lm.name() not in neg_synonyms:
+                            # adds the snonym(s) to the synonyms list
+                            neg_synonyms.append(lm.name())
                 # prints the synonym(s) of the emotional word(s)
-                print("negative syn", set(synonyms))
+                print("negative syn", set(neg_synonyms))
 
             # Input multiple lines of text
-            docs = list(nlp.pipe([text]))
+            # docs = list(nlp.pipe([text]))
 
 
 
@@ -110,5 +131,5 @@ def nlpFunc(text):
     else:
         synonyms = ["Please enter text"]
 
-    return synonyms
+    return  pos_synonyms, neu_synonyms, neg_synonyms
 
